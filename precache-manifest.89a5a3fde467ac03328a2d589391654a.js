2604feb0b7e6196cc592db9dd12c6e9f1d3f9edb
shuklaamar <amarshukla636@gmail.com>
2020-08-23 19:00:56 +0530

    Updates

---
+++ precache-manifest.89a5a3fde467ac03328a2d589391654a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46788c05a090eafe063d897ec485febb",
+    "url": "/class-work-react/index.html"
+  },
+  {
+    "revision": "ec946fc159004313afe4",
+    "url": "/class-work-react/static/css/main.897463d1.chunk.css"
+  },
+  {
+    "revision": "d4c2fc36830b1ad13605",
+    "url": "/class-work-react/static/js/2.0991da06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/class-work-react/static/js/2.0991da06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec946fc159004313afe4",
+    "url": "/class-work-react/static/js/main.dee52233.chunk.js"
+  },
+  {
+    "revision": "050080e67d9bdb2c641a",
+    "url": "/class-work-react/static/js/runtime-main.83be974b.js"
+  }
+]);
